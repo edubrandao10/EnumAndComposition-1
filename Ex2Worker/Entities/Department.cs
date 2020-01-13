be8c441983c8e8002718f616fc3af6bfdc289243
Eduardo Brandao <dusilvabrandao@gmail.com>
2020-01-13 19:47:19 -0300

    Execicie of course C# finished!

---
+++ Ex2Worker/Entities/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex2Worker.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
